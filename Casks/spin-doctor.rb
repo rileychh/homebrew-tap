10499bffa098e7bfae692ec75fbed9f03ccc4cb0
Riley Ho <riley@rileychh.dev>
2026-03-14 17:00:40 +0800

    Fix cask style and add autobump workflow

--- Casks/spin-doctor.rb
+++ Casks/spin-doctor.rb
@@ -1,6 +1,6 @@
 cask "spin-doctor" do
   version "0.2.1"
-  sha256 :no_check
+  sha256 "163904b4c2afa7faf441f30a7db12a0cff2b3008b335281f283e646cefa3fe08"
 
   url "https://github.com/rileychh/spin-doctor/releases/download/v#{version}/SpinDoctor.dmg"
   name "Spin Doctor"
