af452a8aea60a7d347a2f4b4654788d249a95863
Riley Ho <riley@rileychh.dev>
2026-03-14 16:36:30 +0800

    Fix cask stanza order and array style

--- Casks/spin-doctor.rb
+++ Casks/spin-doctor.rb
@@ -7,16 +7,14 @@ cask "spin-doctor" do
   desc "Menu bar app that detects busy-loop processes and offers to kill them"
   homepage "https://github.com/rileychh/spin-doctor"
 
-  app "Spin Doctor.app"
-
-  uninstall quit: "com.spindoctor.app"
-
-  zap trash: [
-    "~/.config/spin_doctor",
-  ]
-
   livecheck do
     url :url
     strategy :github_latest
   end
+
+  app "Spin Doctor.app"
+
+  uninstall quit: "com.spindoctor.app"
+
+  zap trash: "~/.config/spin_doctor"
 end
